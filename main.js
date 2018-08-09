7eba695a690d8b49caf16f091a0de371663b8323
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2018-08-09 00:39:11 -0500

    added new functions like REST API Usage

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const {app,BrowserWindow} = require('electron')
-const url = require('url')
-const path = require('path')
+const {app,BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+const request = require('request');
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
-let win
+let win;
+
 function createWindow(){
-  win = new BrowserWindow({width:800,height:600})
+  win = new BrowserWindow({width:800,height:600});
   win.loadURL(url.format ({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 }
-app.on('ready',createWindow)
+
+app.on('ready',createWindow);
