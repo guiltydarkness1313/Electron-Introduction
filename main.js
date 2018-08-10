eadbcfa6030566383708c8af24cdb4a3cc3ddd5f
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2018-08-09 23:32:07 -0500

    second commit

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const {app,BrowserWindow} = require('electron');
 const url = require('url');
 const path = require('path');
-const request = require('request');
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 let win;
 
 function createWindow(){
-  win = new BrowserWindow({width:800,height:600});
+  win = new BrowserWindow(
+      {
+          titleBarStyle:"hidden",
+          width:1280,
+          height:720,
+          minHeight:600,
+          minWidth:900,
+          backgroundColor:'#FA0E0E',
+          show:true,
+          icon:path.join(__dirname,'public/images/png/256x256.png')
+      }
+      );
+
   win.loadURL(url.format ({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'views/index.html'),
     protocol: 'file:',
     slashes: true
   }))
